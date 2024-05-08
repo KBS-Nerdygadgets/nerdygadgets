7beba66728c785397f142364e77773dbc9263f61
Roeyy <roeygaming07@gmail.com>
2024-05-08 09:44:17 +0200

    minor fix: Spelling

--- Database.java
+++ Database.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class Database {
     public static void main(String[] args) {
-        // JDBC connection parameters
+        // JDBC Connection Parameters
         String url = "jdbc:mysql://localhost:3306/nerdygadgets";
         String username = "root";
         String password = "";
