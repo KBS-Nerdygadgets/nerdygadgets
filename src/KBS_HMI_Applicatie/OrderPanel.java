2ce2918178d55cf8d1c40d91be916b4a06620ac9
Tim-WIN <154435304+Tim-WIN1@users.noreply.github.com>
2024-05-29 12:22:32 +0200

    Update OrderPanel.java

--- src/KBS_HMI_Applicatie/OrderPanel.java
+++ src/KBS_HMI_Applicatie/OrderPanel.java
@@ -192,6 +192,13 @@ public class OrderPanel extends JPanel{
         ArrayList<ArrayList<Integer>> BPPenTSParray = TSP.Main.TSPfuncties(BPParray, OrderID);
         System.out.println(BPPenTSParray);
         System.out.println("");
-        
+        String JavaToArduino = "";
+        for(int rit = 0; rit < BPPenTSParray.size(); rit++){
+            for(int locatie = 0; locatie < BPPenTSParray.get(rit).size(); locatie++){
+                JavaToArduino += BPPenTSParray.get(rit).get(locatie) + ",";
+            }
+            JavaToArduino += "|";
+        }
+        System.out.println(JavaToArduino);
     }
 }
